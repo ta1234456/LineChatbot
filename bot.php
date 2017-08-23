58b2329938bf13f695402d0176d83c298e93447c
seka <natawat_95@hotmail.com>
2017-08-23 20:55:08 +0700

    แก้ไข

--- bot.php
+++ bot.php
@@ -1,7 +1,8 @@
 <?php
 $access_token = 'HwQLf/ZYchE8dejoxTYwzJqcYywX68eahiHhW+DDzTa+sshlRKUSVQZHg854Mf1qGrBuxJgHtEl32jZ1UGXDDroVYSkdO0QL+uqI6ebKUEuI8DEluBLU+YErDDMJQHJoGq5tDF4P8zYa04qWksYMFQdB04t89/1O/w1cDnyilFU=';
-$content = file_get_content('php://input');
-$events=json_decode($content,true);
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
