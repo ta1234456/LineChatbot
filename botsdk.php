10dab6c3331bab5ebb8c047855c9834a6109fc53
seka <natawat_95@hotmail.com>
2017-08-24 20:50:04 +0700

    test

--- botsdk.php
+++ botsdk.php
@@ -1,5 +1,7 @@
 <?php
-
-
-
+$access_token = 'HwQLf/ZYchE8dejoxTYwzJqcYywX68eahiHhW+DDzTa+sshlRKUSVQZHg854Mf1qGrBuxJgHtEl32jZ1UGXDDroVYSkdO0QL+uqI6ebKUEuI8DEluBLU+YErDDMJQHJoGq5tDF4P8zYa04qWksYMFQdB04t89/1O/w1cDnyilFU=';
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+echo $events ;
 ?>
