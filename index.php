47da9740d82a55d31d05e0e82077c9ee945bd124
seka <natawat_95@hotmail.com>
2017-08-25 00:37:36 +0700

    ็PL

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   $contentOnlinePlayer = file_get_contents('https://api.bf4stats.com/api/onlinePlayers');
   $amountplayer='จำนวนคนออนไล';
-  $onlineDecode=json_decode($contentOnlinePlayer);
+  $onlineDecode=json_decode($contentOnlinePlayer,true);
   echo $onlineDecode;
   ?>
