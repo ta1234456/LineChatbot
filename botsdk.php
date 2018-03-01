9d5202db3f8862e7daed4b561d580f815048fa7b
seka <natawat_95@hotmail.com>
2018-03-01 14:04:01 +0700

    Update botsdk.php

--- botsdk.php
+++ botsdk.php
@@ -39,11 +39,11 @@ if (!is_null($events['events'])) {
 			    	'text'=>$amountplayer
 			    	],
 				    ['type'=>'location'
-				     'location'=>$map
-				     "title": "my location",
-   				     "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-    			             "latitude": 35.65910807942215,
-                                     "longitude": 139.70372892916203];
+				     'location'=>$map,
+				     'title'=> 'my location',
+   				     'address'=> '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
+    			             'atitude'=> 35.65910807942215,
+                                     'longitude'=>139.70372892916203];
 
 			    $url = 'https://api.line.me/v2/bot/message/reply';
 			    $data = [
